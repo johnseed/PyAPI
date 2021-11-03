59028c4452d35f477f58bc0ced40db567f78dfcf
John <nowyoutry@outlook.com>
2021-11-03 14:27:08 +0800

    Add client

---
+++ fastapi-client/fastapi-client.py
@@ -0,0 +1,20 @@
+from dapr.clients import DaprClient
+from string import Template
+import time
+
+count = 0
+while True:
+    print("Sending reqest ...")
+    try:
+        time.sleep(1)
+        with DaprClient() as d:
+            # 调用方法 (gRPC 或者 HTTP Get)
+            resp = d.invoke_method('fastapi-server', "/", data='')
+            print(resp.data)
+            # 调用POST方法
+            count += 1
+            data = '{ "email": "%sgda.com",  "password": "%s"}'%(count,count)
+            resp = d.invoke_method('fastapi-server', "/users", data=data, http_verb='POST')
+            print(resp.data)
+    except Exception as e:
+        print(e)
