50e9c02e5a8e6b101ec34a0c2e44542374daf1fa
John <nowyoutry@outlook.com>
2021-11-03 16:29:13 +0800

    invoke method.

--- fastapi-client/fastapi-client.py
+++ fastapi-client/fastapi-client.py
@@ -9,12 +9,14 @@ while True:
         time.sleep(1)
         with DaprClient() as d:
             # 调用方法 (gRPC 或者 HTTP Get)
-            resp = d.invoke_method('fastapi-server', "/", data='')
+            resp = d.invoke_method('fastapi-server', "", data='')
             print(resp.data)
             # 调用POST方法
             count += 1
             data = '{ "email": "%sgda.com",  "password": "%s"}'%(count,count)
-            resp = d.invoke_method('fastapi-server', "/users", data=data, http_verb='POST')
+            resp = d.invoke_method('fastapi-server', "users", data=data, http_verb='POST')
             print(resp.data)
+            # 异常测试
+            resp = d.invoke_method('fastapi-server', "users", data='异常', http_verb='POST')
     except Exception as e:
         print(e)
